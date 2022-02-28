30bd6c828cfff1c4267e24f372cc8f0c1bd3a9a3
Mateus Costa <mm1071259@gmail.com>
2022-02-28 00:17:15 -0300

    Criado os tópicos do menu (6 períodos)

--- app/src/main/java/com/example/pdm2_navigation_drawer_02/MainActivity.java
+++ app/src/main/java/com/example/pdm2_navigation_drawer_02/MainActivity.java
@@ -41,7 +41,13 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_p1,
+                R.id.nav_p2,
+                R.id.nav_p3,
+                R.id.nav_p4,
+                R.id.nav_p5,
+                R.id.nav_p6
+        )
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
---
+++ app/src/main/java/com/example/pdm2_navigation_drawer_02/ui/P2Fragment.java
@@ -0,0 +1,30 @@
+package com.example.pdm2_navigation_drawer_02.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.pdm2_navigation_drawer_02.R;
+
+public class P2Fragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_p2, container, false);
+
+        TextView tvTitle = root.findViewById((R.id.tv_title));
+        tvTitle.setText(R.string.menu_p2);
+
+//        ImageView ivTitle = root.findViewById((R.id.iv_title));
+//        ivTitle.setImageResource(R.drawable.icon_coracao);
+
+        return root;
+    }
+}
